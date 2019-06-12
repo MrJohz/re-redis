81984d94bfcfd4481df31317c080a549eff250f6
Jonathan <jofr@spreadshirt.net>
2019-06-12 12:21:57 +0200

    Add type transforms for RBytes and static arrays

--- src/types/redis_bytes.rs
+++ src/types/redis_bytes.rs
@@ -74,27 +74,48 @@ impl<'a> From<Vec<u8>> for RBytes<'a> {
     }
 }
 
+macro_rules! impl_bytes_for_arrays {
+    ($($N:expr)+) => {
+        $(
+            impl<'a> From<&'a [u8; $N]> for RBytes<'a> {
+                fn from(other: &'a [u8; $N]) -> Self {
+                    RBytes(Cow::from(other.as_ref()))
+                }
+            }
+        )+
+    }
+}
+
+// Okay, for this *alone*, macros are worth their weight in gold...
+impl_bytes_for_arrays!(
+    1 2 3 4 5 6 7 8 9 10
+    11 12 13 14 15 16 17 18 19 20
+    21 22 23 24 25 26 27 28 29 30
+    31 32
+);
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn debug_impl_returns_a_string_if_the_string_can_be_displayed() {
-        let bytes = RBytes::from("test test, this all works");
+    fn it_is_possible_to_create_a_bytes_value_from_a_static_array() {
+        let bytes = RBytes::from(b"this is a bytes array");
 
-        assert_eq!(
-            "RBytes(b\"test test, this all works\")",
-            format!("{:?}", bytes)
-        );
+        assert_eq!(RBytes::from("this is a bytes array"), bytes);
+    }
+
+    #[test]
+    fn debug_impl_returns_a_string_if_the_string_can_be_displayed() {
+        let bytes = RBytes::from("test this all works");
+
+        assert_eq!("RBytes(b\"test this all works\")", format!("{:?}", bytes));
     }
 
     #[test]
     fn debug_impl_returns_a_bytes_array_if_the_string_cannot_be_displayed() {
-        let bytes = RBytes::from(b"\xFF \xF9 \x00".as_ref());
+        let bytes = RBytes::from(b"\xFF\xF9\x00".as_ref());
 
-        assert_eq!(
-            "RBytes([255, 32, 249, 32, 0])",
-            format!("{:?}", bytes)
-        );
+        assert_eq!("RBytes([255, 249, 0])", format!("{:?}", bytes));
     }
 }
